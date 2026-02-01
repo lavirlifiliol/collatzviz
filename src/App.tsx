36b6a15cd279bb518f0825f63c69785400cf744c
Adam Hrbac <27435982+lavirlifiliol@users.noreply.github.com>
2026-02-01 22:58:25 +0100

    Configurable size and some comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// author: xhrbac12
 import { useState, useEffect, type JSX } from 'react'
 import './App.css'
 
@@ -5,14 +6,19 @@ type Update = {
   // ax + b
   rule: 'mul', a: number, b: number
 } | {
+  // x / d
   rule: 'div', d: number
 }
 
+
+// a single rule of the collatz conjecture
 type Rule = {
   angle: number,
   dist: number,
 } & Update
 
+
+// a drawn node of the tree
 type TreeNode = {
   value: number,
   angleHere: number,
@@ -23,12 +29,15 @@ type TreeNode = {
 
 const RAD_OVER_DEG = Math.PI / 180
 
+// move a point over a distance at an angle in degrees
 function shiftPoint(p: {x:number,y:number}, angle: number, dist: number): {x:number,y:number} {
   return {
     x: p.x + Math.cos(angle * RAD_OVER_DEG) * dist,
     y: p.y + Math.sin(angle * RAD_OVER_DEG) * dist,
   }
 }
+
+// Given a TreeNode parent, compute values which would, under the rules, lead to parent.value, and offset the corresponding nodes as per the corresponding rule's angle and dist
 function* treeChildren(rules: Rule[], parent: TreeNode) {
   for (const [i, rule] of rules.entries()) {
     if (rule.rule == 'div') {
@@ -55,9 +64,10 @@ function* treeChildren(rules: Rule[], parent: TreeNode) {
 function drawLine(from: TreeNode, to: TreeNode, ctx: CanvasRenderingContext2D) {
   ctx.moveTo(from.x, from.y)
   ctx.lineTo(to.x, to.y)
-
 }
 
+
+// A single frame of drawing the tree. In order to mitigate lag, this draws at most maxDraw nodes of the tree. work contains TreeNodes whose children haven't been drawn yet, closed contains values which are already on the tree to prevent getting stuck
 function treeAnimationFrame(rules: Rule[], maxDraw: number, maxValue: number, work: TreeNode[], canvas: CanvasRenderingContext2D, closed: Set<number>) {
   for (let i = 0; i < maxDraw && work.length; ++i) {
     const past = work.pop()!
@@ -77,10 +87,12 @@ function treeAnimationFrame(rules: Rule[], maxDraw: number, maxValue: number, wo
   }
 }
 
+// An input element with type number
 function NumericInput(props: {label: string, value: number, setValue: (v: number)=>void}): JSX.Element{
   return (<label>{props.label}<input type="number" className="short" value={props.value} onChange={e=>props.setValue(parseInt(e.target.value))}/></label>)
 }
 
+// Updates a single key of a rule in an array of rules or rule subtypes
 function updateRuleAt<U extends Rule, T extends keyof U>(rules: U[], i: number, key: T, nvalue: U[T]): Rule[] {
   return rules.map((v, j) => j == i?{...v, [key]:nvalue}:v)
 }
@@ -96,16 +108,21 @@ function App() {
   const [maxValue, setMaxValue] = useState(10000)
   const [seed, setSeed] = useState(1)
   const [origin, setOrigin] = useState({x:100, y:700})
-  const fullState = [rules, drawPerFrame, maxValue, seed, origin]
+  const [dim, setDim] = useState(1080);
+
+  // used for undo/redo
+  const fullState = [rules, drawPerFrame, maxValue, seed, origin, dim]
   const setFullState = (t: any) => {
     setRules(t[0])
     setDrawPerFrame(t[1])
     setMaxValue(t[2])
     setSeed(t[3])
     setOrigin(t[4])
+    setDim(t[5])
   }
 
   useEffect(() => {
+    // if the url contains a state, replicate it rather than using the defaults
     let searchParams = new URLSearchParams(location.search)
     let sharedState = searchParams.get("state")
     if (sharedState) {
@@ -115,6 +132,7 @@ function App() {
     }
   }, [])
 
+  // the tree drawing logic.
   useEffect(()=> {
     let work: TreeNode[] = [{value: seed, angleHere: 0, depth: 0, ...origin}];
     let closed = new Set<number>()
@@ -135,11 +153,13 @@ function App() {
     })
     return () => {
       if (handle > -1) {
+        // make sure excessively large trees can be abbandoned before they're done drawing
         cancelAnimationFrame(handle)
       }
     }
   }, fullState)
 
+  // ensure the URL contains the current settings, and store them in history so that we can undo to them
   useEffect(() => {
     const data = JSON.stringify(fullState)
     const b64 = btoa(data)
@@ -150,6 +170,7 @@ function App() {
     }
   }, fullState)
 
+  // If back is executed, restore the state from history
   useEffect(() => {
     const evListener = (ev: PopStateEvent) => {
       setFullState(ev.state)
@@ -158,7 +179,6 @@ function App() {
     return () => removeEventListener("popstate", evListener)
   }, [])
 
-  const DIM = 1080;
 
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
@@ -166,23 +186,42 @@ function App() {
         <canvas 
           style={{ width: "70vmin", height: "70vmin", border: "1px solid darkgrey" }}
           id="tree"
-          width={DIM}
-          height={DIM}
+          width={dim}
+          height={dim}
           onClick={ev=>{
             const canvas = document.getElementById("tree")! as HTMLCanvasElement
             const bb = canvas.getBoundingClientRect()
-            const fix = DIM / bb.width
+            const fix = dim / bb.width
             setOrigin({x: (ev.clientX - bb.left) * fix, y: (ev.clientY - bb.top) * fix})
           }}/>
       </div>
       <NumericInput label="Seed:" value={seed} setValue={setSeed}/>
       <NumericInput label="Cutoff:" value={maxValue} setValue={setMaxValue}/>
       <NumericInput label="Edges per second:" value={drawPerFrame * 60} setValue={v=>setDrawPerFrame(v/60)}/>
+      <NumericInput label="Units per side:" value={dim} setValue={setDim}/>
+      <NumericInput label="Number of rules:" value={rules.length} setValue={v=>{
+        if (v < 2) return;
+        if (v == 2) setRules(collatz);
+        else if (v == 3) setRules([
+          {angle: -10, dist: 5, rule: 'div', d: 3},
+          {angle: 10, dist: 15, rule: 'mul', a: 5, b: 1},
+          {angle: 20, dist: 25, rule: 'mul', a: 5, b: 2},
+        ])
+        else {
+          const nRules: Rule[] = []
+          for (let i = 0; i < v; ++i) {
+            nRules.push({angle: i+1, dist: 5, rule: 'mul', a: 1, b: i})
+          }
+          setRules(nRules)
+        }
+      }} />
       <ul>
         {rules.map((rule, modcls) => {
           if (rule.rule == 'div') {
             return (<li key={modcls}>
-              v<sub>n+1</sub> = v<sub>n</sub>
+              v<sub>n+1</sub> =<span className='swap'
+                onClick={()=>setRules(rules.map((v,i)=>i==modcls?({...v, rule:'mul', a: 1, b: modcls}):v))}
+              >  v<sub>n</sub> </span>
               <NumericInput label=" / " value={rule.d} setValue={v=>setRules(updateRuleAt(rules as (Rule & {rule: 'div'})[], modcls, "d", v))}/>
               if v<sub>n</sub> ≡ {modcls} (mod {rules.length}).
               <NumericInput label="Turn by " value={rule.angle} setValue={v=>setRules(updateRuleAt(rules, modcls, "angle", v))}/>°,
@@ -191,7 +230,8 @@ function App() {
           }
           if (rule.rule == 'mul') {
             return (<li key={modcls}>v<sub>n+1</sub> 
-            <NumericInput label=" = " value={rule.a} setValue={v=>setRules(updateRuleAt(rules as (Rule & {rule: 'mul'})[], modcls, "a", v))}/>v<sub>n</sub>
+            <NumericInput label=" = " value={rule.a} setValue={v=>setRules(updateRuleAt(rules as (Rule & {rule: 'mul'})[], modcls, "a", v))}/>
+              <span className="swap" onClick={()=>setRules(rules.map((v,i)=>i==modcls?{...v, rule:'div', d: 1}:v))}> v<sub>n</sub> </span>
             <NumericInput label=" + " value={rule.b} setValue={v=>setRules(updateRuleAt(rules as (Rule & {rule: 'mul'})[], modcls, "b", v))}/>
               if v<sub>n</sub> ≡ {modcls} (mod {rules.length}).
               <NumericInput label="Turn by " value={rule.angle} setValue={v=>setRules(updateRuleAt(rules, modcls, "angle", v))}/>°,
